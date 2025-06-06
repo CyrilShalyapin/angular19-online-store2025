13e89605f3d966379bdb4ad5fe3227723cb39c72
kiryl.shaliapin <kiryl.shaliapin@gmail.com>
2025-06-06 19:26:44 +0300

    feature: add routing and cart route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProductsListComponent } from './components/products-list/products-list.component';
 import { HeaderComponent } from './components/header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [ProductsListComponent, HeaderComponent],
+  imports: [ProductsListComponent, HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
